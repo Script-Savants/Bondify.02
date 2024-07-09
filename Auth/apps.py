3e356661df06bb5f30a742f20824ab5592560a4b
11FaZe!! <cs-2303029abd-alrahman@akhbaracademy.edu.eg>
2024-07-10 01:21:21 +0300

    Auth created

--- Auth/apps.py
+++ Auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NewaccountConfig(AppConfig):
+class AuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NewAccount'
+    name = 'Auth'
