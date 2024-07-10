ab16e0756e2db372c5244d1d35dc58d9515294c7
11FaZe!! <cs-2303029abd-alrahman@akhbaracademy.edu.eg>
2024-07-10 18:36:51 +0300

    model created

--- Auth/models.py
+++ Auth/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=10, choices=(('Male', 'Male'), ('Female', 'Female')))
+    bio = models.TextField(max_length=100, blank=True)
     age = models.PositiveIntegerField()
     location = models.CharField(max_length=100)
